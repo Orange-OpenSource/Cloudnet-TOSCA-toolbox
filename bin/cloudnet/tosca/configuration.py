ce14bebea149bd4153c4d9fc6c1493ec547dc706
Xavier-Frédéric Moulet <xavier-frederic.moulet@orange.com>
2021-03-15 12:04:26 +0100

    fix for deleting subkeys configuration instead of keys

--- bin/cloudnet/tosca/configuration.py
+++ bin/cloudnet/tosca/configuration.py
@@ -32,7 +32,11 @@ def load(config_file=CONFIGURATION_FILE, ignored_keys=[]):
         # Load the configuration file if it exists.
         with open(config_file, "r") as stream:
             content = yaml.safe_load(stream)
-            for k in ignored_keys : del content[k]
+            # delete all subkeys of content which are in ignored values
+            for d in content.values() : 
+                if isinstance(d,dict) : 
+                    for k in ignored_keys : 
+                        if k in d: del d[k]
             configuration = merge_dict(DEFAULT_CONFIGURATION, content)
 
     # Configure logging.
