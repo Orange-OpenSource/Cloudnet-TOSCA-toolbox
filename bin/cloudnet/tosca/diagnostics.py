5594f37a93dfca8c516a6d0fd7387b185630f441
Jean-Luc Coulin <63356162+JLCoulin@users.noreply.github.com>
2021-03-29 14:57:12 +0200

    Update diagnostics.py
    Convert indentation tab to space

--- bin/cloudnet/tosca/diagnostics.py
+++ bin/cloudnet/tosca/diagnostics.py
@@ -7,19 +7,19 @@ outfile = None  # file object to output to
 template = ""  # name of the template file
 return_code = 0  # all OK by default
 
-def configure(template_filename, log_filename) : 
-	global outfile, template
-	template = template_filename
-	if log_filename : 
-		outfile = open(log_filename, "a")
+def configure(template_filename, log_filename) :
+    global outfile, template
+    template = template_filename
+    if log_filename :
+        outfile = open(log_filename, "a")
 
 def diagnostic(gravity, file, message, cls, **kwargs) :
-	global return_code, outfile, template
-	if gravity == "info" or outfile == None:
+    global return_code, outfile, template
+    if gravity == "info" or outfile == None:
         return
-	return_code = 1
-	if file == "":
-		file = template
-	kwargs.update(gravity=gravity, file=file, message=message, cls=cls)
-	json.dump(kwargs, outfile, skipkeys=True)
-	outfile.write("\n") # message separator
+    return_code = 1
+    if file == "":
+        file = template
+    kwargs.update(gravity=gravity, file=file, message=message, cls=cls)
+    json.dump(kwargs, outfile, skipkeys=True)
+    outfile.write("\n") # message separator
