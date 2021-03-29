0bd7a505dcbb40aba5c6f19413e5b032767558ef
Jean-Luc Coulin <63356162+JLCoulin@users.noreply.github.com>
2021-03-29 11:46:25 +0200

    Update diagnostics.py
    Applied linter format recommandations

--- bin/cloudnet/tosca/diagnostics.py
+++ bin/cloudnet/tosca/diagnostics.py
@@ -3,22 +3,23 @@ import json
 import sys
 import os
 
-outfile = None # file object to output to
+outfile = None  # file object to output to
 template = ""  # name of the template file
-return_code=0  # all OK by default
+return_code = 0  # all OK by default
 
 def configure(template_filename, log_filename) : 
 	global outfile, template
-	template=template_filename
+	template = template_filename
 	if log_filename : 
-		outfile=open(log_filename,'a')
+		outfile = open(log_filename, "a")
 
 def diagnostic(gravity, file, message, cls, **kwargs) :
 	global return_code, outfile, template
-	if gravity=='info' or outfile == None : return
+	if gravity == "info" or outfile == None:
+        return
 	return_code = 1
 	if file == "":
 		file = template
 	kwargs.update(gravity=gravity, file=file, message=message, cls=cls)
 	json.dump(kwargs, outfile, skipkeys=True)
-	outfile.write('\n') # message separator
+	outfile.write("\n") # message separator
