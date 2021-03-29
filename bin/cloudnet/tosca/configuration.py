fc908d572c4e47fe9dc67cff2bdb8b695baec29f
Jean-Luc Coulin <63356162+JLCoulin@users.noreply.github.com>
2021-03-29 13:23:23 +0200

    Update configuration.py
    Applied missed linter format recommandations

--- bin/cloudnet/tosca/configuration.py
+++ bin/cloudnet/tosca/configuration.py
@@ -33,8 +33,8 @@ def load(config_file=CONFIGURATION_FILE, ignored_keys=[]):
         with open(config_file, "r") as stream:
             content = yaml.safe_load(stream)
             # delete all subkeys of content which are in ignored values
-            for d in content.values() : 
-                if isinstance(d, dict) : 
+            for d in content.values(): 
+                if isinstance(d, dict): 
                     for k in ignored_keys: 
                         if k in d:
                             del d[k]
