4450892a2e51cc84026ab2b5063b0174bdeac959
Philippe Merle <philippe.merle@inria.fr>
2022-04-16 15:32:31 +0200

    Avoided an exception

--- bin/cloudnet/tosca/alloy.py
+++ bin/cloudnet/tosca/alloy.py
@@ -996,7 +996,7 @@ class AbstractAlloySigGenerator(Generator):
                 property_name_format % utils.normalize_name(property_name)
             )
             property_value = template_properties.get(property_name)
-            if property_value is None:
+            if property_value is None and property_declaration is not None:
                 # if the property is unassigned then get the value for the property definition
                 property_value = property_declaration.get("value")
             if property_value is not None:
